94850cda338c51ebb272641b385d608499509c8c
Jorge Eduardo Jiménez Ramírez <Balmund@Thaumazo.lan>
2024-08-02 23:31:58 -0400

    New Changes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import HomeHeader from "../components/HomeHeader";
 import celestialSphere from "../assets/celestialSphere.jpeg"
 import "./HomePage.css";
+import starlitOneLogo from '../assets/starlitOneLogo.png';
 
 function HomePage() {
   return (
@@ -12,11 +13,18 @@ function HomePage() {
     </header>
     <section className = "home-page">
     <div>
-      <img src={celestialSphere} className= "cetestial-sphere"/>
+    <img src={celestialSphere} className= "cetestial-sphere"/>
       <h1>Starlit One Catalog</h1>
       <p>...Endeavoring pathways toward the stars</p>
+      <br/>
     </div>
     </section>
+    <section className= "container">
+    </section>
+    <footer>
+      <p>Copyright © 2024 Starlit One Catalog
+      </p>
+    </footer>
     </body>
   );
 }
--- src/pages/StarPatternsPage.jsx
+++ src/pages/StarPatternsPage.jsx
@@ -1,14 +1,10 @@
-import StarPatternPageHeader from '../components/StarPatternPageHeader';
+import StarPatternPageHeader from "../components/StarPatternPageHeader";
 
-function StarPatternsPage(){
-
-
-    
-        return (
-            <div>
-                <StarPatternPageHeader></StarPatternPageHeader>
-            </div>
-        );
-
+function StarPatternsPage() {
+  return (
+    <div>
+      <StarPatternPageHeader></StarPatternPageHeader>
+    </div>
+  );
 }
 export default StarPatternsPage;
